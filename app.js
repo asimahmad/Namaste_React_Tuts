13c568eb6ef3b44cdc4f40ad38adba304e47d077
Shakeel, Asim <Asim.Shakeel@bshg.com>
2023-05-02 14:36:52 +0530

    Div Siblings under parent

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const parent = React.createElement('div',{id:'parent'},
-React.createElement('div',{id:'child'}, 
 [
-    React.createElement('h1',{}, 'I am an H1 tag'),
-    React.createElement('h2', {}, 'I am an H2 tag')
-])
-);
+    React.createElement('div',{id:'child1'}, 
+    [
+        React.createElement('h1',{}, 'I am an H1 tag from child1'),
+        React.createElement('h2', {}, 'I am an H2 tag from child1')
+    ]),
+    React.createElement('div',{id:'child2'}, 
+    [
+        React.createElement('h1',{}, 'I am an H1 tag from child2'),
+        React.createElement('h2', {}, 'I am an H2 tag from child2')
+    ])
+]);
 
 
 

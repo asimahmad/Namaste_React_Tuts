4e71d5550ef3826a75dd0357100acc6426578243
Shakeel, Asim <Asim.Shakeel@bshg.com>
2023-05-02 14:31:55 +0530

    Siblings tags in react

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const parent = React.createElement('div',{id:'parent'},
- React.createElement('div',{id:'child'}, 
- React.createElement('h1',{}, 'I am an H1 tag')));
+React.createElement('div',{id:'child'}, 
+[
+    React.createElement('h1',{}, 'I am an H1 tag'),
+    React.createElement('h2', {}, 'I am an H2 tag')
+])
+);
 
 
 

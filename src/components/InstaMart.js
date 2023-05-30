687a4e8240559b9a46852abd1fba522136c7fd90
Shakeel, Asim <Asim.Shakeel@bshg.com>
2023-05-30 15:27:53 +0530

    State lifting with more optimize solution

--- src/components/InstaMart.js
+++ src/components/InstaMart.js
@@ -17,67 +17,44 @@ const Section = ({title,desc, isDisplay, setDisplay}) =>{
 }
 
 const InstaMart = ()=>{
-    const [sectionConfig, setSectionConfig] = useState({
-        showAbout: false,
-        showTeam: false,
-        showCareers: false
-    });
+    // const [sectionConfig, setSectionConfig] = useState({
+    //     showAbout: false,
+    //     showTeam: false,
+    //     showCareers: false
+    // });
+    const [visibleSection, setVisibleSection ] = useState('');
 
     return (
     <>
         <h1 className='text-3xl p-2 m-2 font-bold'>InstaMart</h1>
         <Section 
-        isDisplay={sectionConfig.showAbout} 
+        isDisplay={visibleSection === 'about'} 
         setDisplay={()=>{
-            sectionConfig.showAbout?
-            setSectionConfig({
-                showAbout: false,
-                showTeam: false,
-                showCareers: false
-            }):
-            setSectionConfig({
-                showAbout: true,
-                showTeam: false,
-                showCareers: false
-            })
+            visibleSection==='about'?
+            setVisibleSection(''):
+            setVisibleSection('about')
         }} 
         title={'About InstaMart'} 
         desc={'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.'}
         />
 
         <Section 
-        isDisplay={sectionConfig.showTeam}
+        isDisplay={visibleSection === 'team'}
         setDisplay={()=>{
-            sectionConfig.showTeam?
-            setSectionConfig({
-                showAbout: false,
-                showTeam: false,
-                showCareers: false
-            }):
-            setSectionConfig({
-                showAbout: false,
-                showTeam: true,
-                showCareers: false
-            })
+            visibleSection==='team'?
+            setVisibleSection(''):
+            setVisibleSection('team')
         }} 
         title={'Team InstaMart'} 
         desc={'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'}
         />
 
         <Section
-        isDisplay={sectionConfig.showCareers}
+        isDisplay={visibleSection === 'careers'}
         setDisplay={()=>{
-            sectionConfig.showCareers?
-            setSectionConfig({
-                showAbout: false,
-                showTeam: false,
-                showCareers: false
-            }):
-            setSectionConfig({
-                showAbout: false,
-                showTeam: false,
-                showCareers: true
-            })
+            visibleSection==='careers'?
+            setVisibleSection(''):
+            setVisibleSection('careers')
         }} 
         title={'Careers'} 
         desc={'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'}
